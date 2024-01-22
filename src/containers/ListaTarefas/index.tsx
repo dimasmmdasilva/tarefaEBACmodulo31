1c5c6370d7dcf8903fa8797208fc84cd66b5dcf8
Dimas_Marcondes <dimasmarcondes88@gmail.com>
2024-01-22 17:13:01 -0300

    atualizando tarefa EBAC módulo 31 REACT REDUX STYLED

---
+++ src/containers/ListaTarefas/index.tsx
@@ -0,0 +1,26 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+const ListaTarefas = () => (
+  <Container>
+    <p>2 tarefas marcadas como: &quot;categoria&ldquo; &quot;termo&ldquo;</p>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
+  </Container>
+)
+export default ListaTarefas
